29800121ed26d02102e8ade852143d3a8d077333
Shoham <shozeh123@gmail.com>
2022-07-18 17:34:12 +0300

    added cylinder

--- complexShapes/StreetLamp.cpp
+++ complexShapes/StreetLamp.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "copmlexShapes/StreetLamp.h"
+#include "geometries/Cylinder.h"
 
 StreetLamp::StreetLamp(Point point, double height, Color color, Vector up, double radius) : light(
         PointLight(point.add(up.normalize().scale(height + radius * 3)), color)
@@ -11,8 +12,8 @@ Sphere temp = Sphere(point.add(up.normalize().scale(height + radius *3)),radius 
         .setEmission(color)
         .setMaterial(Material().setTransparent(1));
     this->geometries.addShared(std::make_shared<Sphere>(temp));
-    //this->geometries.add(std::make_shared<Cylinder>(Cylinder(Ray(point, up), radius, height)
-    //.setEmission(Color(127, 127, 127).scale(1.25))
-    //.setMaterial(Material().setTransparent(0.3))));
+    this->geometries.addShared(std::make_shared<Cylinder>(Cylinder(Ray(point, up), radius, height)
+    .setEmission(Color(127, 127, 127).scale(1.25))
+    .setMaterial(Material().setTransparent(0.3))));
 
 }
